5f2388dea94b2c998d2f63059a6268b1ca1cabf0
bearics <bearics@gmail.com>
2018-06-09 15:31:37 +0900

    Init solution

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Paint
 {
-    partial class Form1
+    partial class 그림판
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // 그림판
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "그림판";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Paint
 {
-    public partial class Form1 : Form
+    public partial class 그림판 : Form
     {
-        public Form1()
+        public 그림판()
         {
             InitializeComponent();
         }
